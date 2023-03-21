27badc15b7829b7bf6deee872ab17da0dd2747a7
matejko94 <matej.senozetnik@gmail.com>
2023-03-21 13:07:52 +0100

    update page

--- popup.js
+++ popup.js
@@ -3,3 +3,7 @@ function myFunction() {
     var popup = document.getElementById("myPopup");
     popup.classList.toggle("show");
 }
+function myFunction2() {
+    var popup = document.getElementById("myPopup2");
+    popup.classList.toggle("show");
+}
